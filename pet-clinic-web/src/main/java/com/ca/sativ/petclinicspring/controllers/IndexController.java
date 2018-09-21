876febdb9b96a9de0ed153aae4fa5d1f8d41870d
Ivan Satsiuk <sativ01@ca.com>
2018-09-21 17:08:00 +0200

    Created a Index page and controller. Closes #6

---
+++ pet-clinic-web/src/main/java/com/ca/sativ/petclinicspring/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.ca.sativ.petclinicspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "templates/index.html"})
+    public String index() {
+        return "index";
+    }
+}
