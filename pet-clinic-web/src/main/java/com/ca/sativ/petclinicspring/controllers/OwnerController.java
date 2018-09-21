aaf133bdab7568b660b5bf14ead66cc68fb2f076
Ivan Satsiuk <sativ01@ca.com>
2018-09-21 17:29:42 +0200

    Implemented index page and controller for Owners. Closes #9

--- pet-clinic-web/src/main/java/com/ca/sativ/petclinicspring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ca/sativ/petclinicspring/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package com.ca.sativ.petclinicspring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("vets")
+@RequestMapping("owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets(){
-        return "vets/index";
+        return "owners/index";
     }
 }
